9eefd6148412fa10421b56f3ef4b2c739a0b12bf
Wayne <kleavonwayne6270@gmail.com>
2025-05-07 12:09:03 +0200

    fix minor issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import Inventory from "./features/inventory/components/Inventory";
 function App() {
   return (
     <div className="relative w-screen min-h-screen bg-gradient-to-t from-[#212a55] via-[#181f3d] to-[#131831] ">
-      <div className="flex flex-col lg:flex-row gap-y-20 justify-around w-full items-center">
+      <div className="flex justify-around w-full items-center">
         <Shop />
-
         <Inventory />
       </div>
     </div>
